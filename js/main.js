21825f2fd0fa397c80cd13cf51f15f831a28d884
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-10-03 18:26:52 +0200

    moon animation working

--- js/main.js
+++ js/main.js
@@ -7,11 +7,11 @@ let moon = "";
 function init() {
   console.log("init");
 
-  //let moon = document.querySelector("#moon");
-  //TweenMax.to(moon, 5, { y: -300, x: 100, ease: Power1.easeInOut, delay: 1 });
+  let moon = document.querySelector("#moon");
+  TweenMax.to(moon, 8, { y: -500, x: 100, ease: Power1.easeOut, delay: 1 });
 
-  ScrollReveal().reveal(".white-reveal", { delay: 500 });
-  ScrollReveal().reveal(".blue-reveal", { delay: 500 });
+  ScrollReveal().reveal(".white-reveal", { delay: 300 });
+  ScrollReveal().reveal(".blue-reveal", { delay: 300 });
 
   window.onscroll = function() {
     let navScrollMob = document.querySelector(".header");
