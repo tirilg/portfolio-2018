a9e6456d02b4a32339a7dfae7fddb1b7d0ee0fb0
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-10-03 16:20:27 +0200

    logo and burger menu grid works

--- js/main.js
+++ js/main.js
@@ -2,8 +2,13 @@
 
 window.addEventListener("DOMContentLoaded", init);
 
+let moon = "";
+
 function init() {
   console.log("init");
+
+  //let moon = document.querySelector("#moon");
+  //TweenMax.to(moon, 5, { y: -300, x: 100, ease: Power1.easeInOut, delay: 1 });
 
   ScrollReveal().reveal(".white-reveal", { delay: 500 });
   ScrollReveal().reveal(".blue-reveal", { delay: 500 });
