1be1383759149a3b6d3c11713ac9cb0653b46d4d
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-10-05 16:19:26 +0200

    arrow sorta works. working on footer

--- js/animation.js
+++ js/animation.js
@@ -7,6 +7,10 @@ let moon = "";
 function init() {
   console.log("init");
 
+  let arrow = document.querySelector(".arrow");
+  TweenMax.to(arrow, 1, { y: -20 });
+  TweenMax.to(arrow, 1, { y: 0, delay: 1 });
+
   let moon = document.querySelector("#moon");
   TweenMax.to(moon, 8, { y: -500, x: 100, ease: Power1.easeOut, delay: 1 });
 }
