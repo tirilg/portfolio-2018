13b087e1410dd3e99d0f902f4cc732593cda45cd
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-10-05 16:51:12 +0200

    fixed arrow

--- js/animation.js
+++ js/animation.js
@@ -8,7 +8,7 @@ function init() {
   console.log("init");
 
   let arrow = document.querySelector(".arrow");
-  TweenMax.to(arrow, 1, { y: -20 });
+  TweenMax.to(arrow, 1, { y: -10 });
   TweenMax.to(arrow, 1, { y: 0, delay: 1 });
 
   let moon = document.querySelector("#moon");
