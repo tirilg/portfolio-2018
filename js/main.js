fd855db3e4105dec2d48b83ecebda9d08ba3f337
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-10-06 18:27:19 +0200

    added overlays to images on projects page

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  ScrollReveal({ reset: true });
-  ScrollReveal().reveal(".white-reveal", { delay: 300 });
-  ScrollReveal().reveal(".blue-reveal", { delay: 300 });
+  //ScrollReveal({ reset: true });
+  ScrollReveal().reveal(".white-reveal", { delay: 300, reset: true });
+  ScrollReveal().reveal(".blue-reveal", { delay: 300, reset: true });
+  ScrollReveal().reveal(".container", { delay: 100 });
+  ScrollReveal().reveal(".image-text", { delay: 300 });
 
   window.onscroll = function() {
     const navScrollBg = document.querySelector(".header");
